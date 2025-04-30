fadb989d846c18c1b17f93785b774d6b44558d93
ArianaSaenz <nohemysanz801@gmail.com>
2025-04-29 23:44:29 -0600

    lo demás del proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <div>
+
+
+            <h1 style={{ fontSize: '3rem', fontWeight: 'bold', textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)' }}>
+                ¡Bienvenidos a Bocadito!
+            </h1>
+            <p style={{ fontSize: '1.5rem', marginTop: '10px', textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)' }}>
+                "Donde los sabores se encuentran con la felicidad"
+            </p>
+        </div>
+    )
+}
+
+export default Home;
